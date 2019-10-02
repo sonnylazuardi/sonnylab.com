694197a1c906135ddbaaf4d920b60d91637248dd
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2019-10-02 16:42:48 +0800

    Add typescript and antd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from "react"
+import { Button } from "antd"
 
 export default () => (
   <div>
     <h1>Hello world!</h1>
     <p>lorem ipsum dolor sit amet</p>
+    <Button type="primary">Primary</Button>
   </div>
 )
